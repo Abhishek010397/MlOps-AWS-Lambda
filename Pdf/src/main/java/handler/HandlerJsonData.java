6bf2c9820e468d1660b6b5f358231f52c1019e48
Abhishek Dasgupta <55708520+Abhishek010397@users.noreply.github.com>
2021-04-29 05:02:21 +0530

    Exception Solved PdfToImage Conversion Successfull

--- Pdf/src/main/java/handler/HandlerJsonData.java
+++ Pdf/src/main/java/handler/HandlerJsonData.java
@@ -20,11 +20,6 @@ import java.util.*;
 
 public class HandlerJsonData {
     public String handleRequest(Map<String, Object> event, Context context) throws IOException {
-        //Java Code takes Json with List, Then it converts the json list to ArrayList,
-        //It also writing to /tmp/
-        //It calls Pdfbox to convert .pdf to .png
-        //it also uploading .png to s3
-        //it will sent chunks to downstream
         String bucket;
         String folder;
         try {
@@ -45,34 +40,18 @@ public class HandlerJsonData {
             for (int i = 0; i < pdfs.size(); i++) {
                 String key = pdfs.get(i);
                 System.out.println("Key is: "+key);
-                String newKey = key.replaceAll("[^\\d]", " ");
-                newKey = newKey.trim();
-                Integer index = Integer.parseInt(newKey);
-                //System.out.println(index);
-                String key_Name = folder + key; //extracted-pdf/document-page0.pdf
+                String key_Name = folder + key;
                 System.out.println("Folder is: "+key_Name);
-                 //* key information is coming from json
-                 //* Iam appending that key with folder and asking s3 to give me the content of that
                  GetObjectRequest getObjectRequest = new GetObjectRequest(bucket, key_Name);
                  S3Object s3Object = s3client.getObject(getObjectRequest);
                  InputStream fileInputStream = s3Object.getObjectContent();
-                 File newFile = new File("/tmp/" + key);//key = document-page0.pdf
+                 File newFile = new File("/tmp/" + key);
                  FileUtils.copyInputStreamToFile(fileInputStream, newFile);
-                 /**
-                 BufferedReader br = new BufferedReader(new FileReader(newFile));
-                 int j =0;
-                 String content;
-                 while((content = br.readLine())!= null)
-                     System.out.println(j++);
-                 try {
-                  **/
                  try (final PDDocument document = PDDocument.load(newFile)) {
-                     //String fileName = "document-page-" + "page" + ".png";
-                     String[] keyName = key.split(".pdf");//{"document-page-0"}
+                     String[] keyName = key.split(".pdf");
                      for(String KeyValue : keyName) {
                          PDFRenderer pdfRenderer = new PDFRenderer(document);
-                         BufferedImage img = pdfRenderer.renderImageWithDPI(index, 300, ImageType.RGB);
-                         System.out.println("Index is: "+index);
+                         BufferedImage img = pdfRenderer.renderImageWithDPI(0, 300, ImageType.RGB);
                          System.out.println("Splitted Key is: "+KeyValue);
                          ImageIOUtil.writeImage(img, "/tmp/" + KeyValue + ".png", 300);
                          System.out.println("Output KeyValue is: "+KeyValue+".png");
