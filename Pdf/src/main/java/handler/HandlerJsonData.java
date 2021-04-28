c3ae0b558cfb57ffa676af5efe4ffe610032f76a
Abhishek010397 <abhishekdg7880@gmail.com>
2021-04-28 23:40:58 +0530

    Index Out Of Bound Exception Testing

--- Pdf/src/main/java/handler/HandlerJsonData.java
+++ Pdf/src/main/java/handler/HandlerJsonData.java
@@ -1,5 +1,4 @@
 package handler;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -20,7 +19,12 @@ import java.io.*;
 import java.util.*;
 
 public class HandlerJsonData {
-    public String handleRequest(Map<String, Object> event, Context context) {
+    public String handleRequest(Map<String, Object> event, Context context) throws IOException {
+        //Java Code takes Json with List, Then it converts the json list to ArrayList,
+        //It also writing to /tmp/
+        //It calls Pdfbox to convert .pdf to .png
+        //it also uploading .png to s3
+        //it will sent chunks to downstream
         String bucket;
         String folder;
         try {
@@ -40,39 +44,53 @@ public class HandlerJsonData {
             }
             for (int i = 0; i < pdfs.size(); i++) {
                 String key = pdfs.get(i);
-                System.out.println(key);
-                String key_Name = folder + key;
-                System.out.println(key_Name);
-                GetObjectRequest getObjectRequest = new GetObjectRequest(bucket, key_Name);
-                S3Object s3Object = s3client.getObject(getObjectRequest);
-                InputStream fileInputStream = s3Object.getObjectContent();
-                File newFile = new File("/tmp/"+key);
-                FileUtils.copyInputStreamToFile(fileInputStream, newFile);
-                try {
-                    try (final PDDocument document = PDDocument.load(newFile)) {
-                        PDFRenderer pdfRenderer = new PDFRenderer(document);
-                        for (int page = 0; page < document.getNumberOfPages(); ++page) {
-                            BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
-                            String fileName = "document-page-" + page + ".png";
-                            ImageIOUtil.writeImage(bim, "/tmp/document-page-" + page + ".png", 300);
-                            s3client.putObject(
-                                    bucket,
-                                    "extracted-image/"+fileName,
-                                    new File("/tmp/" + fileName)
-                            );
-                        }
-                        document.close();
-                    } catch (IOException e) {
-                        System.err.println("Exception while trying to create pdf document - " + e);
-                    }
-                } catch (AmazonServiceException e) {
-                    System.err.println(e.getErrorMessage());
-                    System.exit(1);
-                }
+                System.out.println("Key is: "+key);
+                String newKey = key.replaceAll("[^\\d]", " ");
+                newKey = newKey.trim();
+                Integer index = Integer.parseInt(newKey);
+                //System.out.println(index);
+                String key_Name = folder + key; //extracted-pdf/document-page0.pdf
+                System.out.println("Folder is: "+key_Name);
+                 //* key information is coming from json
+                 //* Iam appending that key with folder and asking s3 to give me the content of that
+                 GetObjectRequest getObjectRequest = new GetObjectRequest(bucket, key_Name);
+                 S3Object s3Object = s3client.getObject(getObjectRequest);
+                 InputStream fileInputStream = s3Object.getObjectContent();
+                 File newFile = new File("/tmp/" + key);//key = document-page0.pdf
+                 FileUtils.copyInputStreamToFile(fileInputStream, newFile);
+                 /**
+                 BufferedReader br = new BufferedReader(new FileReader(newFile));
+                 int j =0;
+                 String content;
+                 while((content = br.readLine())!= null)
+                     System.out.println(j++);
+                 try {
+                  **/
+                 try (final PDDocument document = PDDocument.load(newFile)) {
+                     //String fileName = "document-page-" + "page" + ".png";
+                     String[] keyName = key.split(".pdf");//{"document-page-0"}
+                     for(String KeyValue : keyName) {
+                         PDFRenderer pdfRenderer = new PDFRenderer(document);
+                         BufferedImage img = pdfRenderer.renderImageWithDPI(index, 300, ImageType.RGB);
+                         System.out.println("Index is: "+index);
+                         System.out.println("Splitted Key is: "+KeyValue);
+                         ImageIOUtil.writeImage(img, "/tmp/" + KeyValue + ".png", 300);
+                         System.out.println("Output KeyValue is: "+KeyValue+".png");
+                         document.close();
+                         s3client.putObject(
+                                 bucket,
+                                 "extracted-image/" + KeyValue + ".png",
+                                 new File("/tmp/" + KeyValue + ".png")
+                         );
+                     }
+                 } catch (IOException e) {
+                 System.err.println("Exception while trying to create pdf document - " + e);
+                 }
             }
-        }catch (JSONException | IOException e) {
+            }catch (JSONException e) {
             throw new JSONException(e);
         }
         return null;
     }
 }
+
